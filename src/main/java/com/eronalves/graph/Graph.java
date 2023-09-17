a02b7a6d00d49ec8d174f22eda41872f9629ceef
EronAlves1996 <eron.alves@rocketmail.com>
2023-09-17 14:08:07 -0300

    implement depth first with recursion

--- src/main/java/com/eronalves/graph/Graph.java
+++ src/main/java/com/eronalves/graph/Graph.java
@@ -1,56 +1,73 @@
 package com.eronalves.graph;
 
 import java.util.*;
+import java.util.function.Supplier;
 
 public class Graph<T> {
 
-    HashMap<String, Node<T>> nodes;
+  HashMap<String, Node<T>> nodes;
 
-    public Graph(HashMap<String, Node<T>> nodes) {
-        this.nodes = nodes;
+  public Graph (HashMap<String, Node<T>> nodes) {
+    this.nodes = nodes;
+  }
+
+  public Graph () {
+    this.nodes = new HashMap<>();
+  }
+
+  public List<T> traverseDepthFirst (String startFrom) {
+    var list = new ArrayList<T>();
+    var stack = new Stack<Node<T>>();
+
+    if (nodes.containsKey(startFrom)) {
+      stack.push(nodes.get(startFrom));
+
+      while (!stack.empty()) {
+        Node<T> lastNode = stack.pop();
+        list.add(lastNode.value);
+
+        lastNode.edges.forEach(edge -> stack.push(edge.get()));
+      }
     }
 
-    public Graph() {
-        this.nodes = new HashMap<>();
+    return Collections.unmodifiableList(list);
+  }
+
+  public List<T> traverseDepthFirstRecursive (String startFrom) {
+    var list = new ArrayList<T>();
+
+    if (nodes.containsKey(startFrom)) {
+      traverseDepthFirstRecursive(nodes.get(startFrom), list);
     }
 
-    public List<T> traverseDepthFirst(String startFrom) {
-        var list = new ArrayList<T>();
-        var stack = new Stack<Node<T>>();
+    return list;
+  }
 
-        if (nodes.containsKey(startFrom)) {
-            Node<T> node = nodes.get(startFrom);
-            stack.push(node);
+  private void traverseDepthFirstRecursive (Node<T> node, List<T> list) {
+    list.add(node.value);
 
-            while (!stack.empty()) {
-                Node<T> lastNode = stack.pop();
-                list.add(lastNode.value);
+    for (Supplier<Node<T>> edge : node.edges) {
+      traverseDepthFirstRecursive(edge.get(), list);
+    }
+  }
 
-                lastNode.edges.forEach(edge -> stack.push(edge.get()));
-            }
-        }
+  public List<T> traverseBreathFirst (String starFrom) {
+    var list = new ArrayList<T>();
+    Deque<Node<T>> queue = new LinkedList<>();
 
-        return Collections.unmodifiableList(list);
+    if (nodes.containsKey(starFrom)) {
+      queue.push(nodes.get(starFrom));
+
+      while (!queue.isEmpty()) {
+        Node<T> firstNode = queue.pollFirst();
+        list.add(firstNode.value);
+
+        firstNode.edges.forEach(edge -> queue.add(edge.get()));
+      }
     }
 
-    public List<T> traverseBreathFirst(String starFrom) {
-        var list = new ArrayList<T>();
-        Deque<Node<T>> queue = new LinkedList<>();
+    return Collections.unmodifiableList(list);
 
-        if (nodes.containsKey(starFrom)) {
-            Node<T> node = nodes.get(starFrom);
-            queue.push(node);
-
-            while (!queue.isEmpty()) {
-                Node<T> firstNode = queue.pollFirst();
-                list.add(firstNode.value);
-
-                firstNode.edges.forEach(edge -> queue.add(edge.get()));
-            }
-        }
-
-        return Collections.unmodifiableList(list);
-
-    }
+  }
 
 }
